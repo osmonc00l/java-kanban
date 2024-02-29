e8a9e7d3b1f10e413ae46abfab8e18643f9d8a7a
raijin <bosmkul@gmail.com>
2024-03-01 00:32:12 +0300

    4th-sprint | fix after second review

---
+++ src/ru/yandex/schedule/tasks/Status.java
@@ -0,0 +1,7 @@
+package ru.yandex.schedule.tasks;
+
+public enum Status {
+    NEW,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/ru/yandex/schedule/tasks/Subtask.java
@@ -0,0 +1,27 @@
+package ru.yandex.schedule.tasks;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "ru.yandex.schedule.Tasks.Subtask{" +
+                "EpicId=" + epicId +
+                ", name='" + super.getName() + '\'' +
+                ", description='" + super.getDescription() + '\'' +
+                ", id=" + super.getId() +
+                ", status=" + super.getStatus() +
+                '}';
+    }
+
+
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+}
