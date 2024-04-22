74d2242f69769f9d24f800c8b3fa40229e4001a1
raijin <bosmkul@gmail.com>
2024-04-23 00:44:11 +0300

    6th-sprint | quit if element == null

--- src/ru/yandex/schedule/manager/FileBackedTaskManager.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.schedule.manager;
-
-public class FileBackedTaskManager extends InMemoryTaskManager{
-    public FileBackedTaskManager(HistoryManager historyManager) {
-        super(historyManager);
-    }
-}
